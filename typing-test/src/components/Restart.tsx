cd057b050b45175622596ce5c3867a31d56baace
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-03-17 17:13:47 +0530

    update

--- typing-test/src/components/Restart.tsx
+++ typing-test/src/components/Restart.tsx
@@ -16,8 +16,8 @@ const Restart = ({
     }
 
     return (
-        <button onClick={handleRestart} className={`block rounded px-8 py-2 hover:bg-slate-700/50 keyboard ${className}`}>
-            <MdRefresh className="w-6 h-6"/>
+        <button onClick={handleRestart} className={`block rounded-2xl px-12 py-3 hover:bg-gray-800 hover:text-white ${className}`}>
+            <MdRefresh className="w-6 h-7"/>
         </button>
     )
 }
