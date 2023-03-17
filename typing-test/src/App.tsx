33763306d59eaff6ed1aa69e6e6b33b80888f338
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-03-17 17:13:37 +0530

    update

--- typing-test/src/App.tsx
+++ typing-test/src/App.tsx
@@ -1,9 +1,9 @@
-import { faker } from "@faker-js/faker";
 import Restart from "./components/Restart";
 import Results from "./components/Results";
 import Typing from './components/Typing';
 import useEngine from "./hooks/useEngine";
 import { calculateAccuracyPercentage } from './utils/helpers';
+import Picture from './assets/pic5.jpg'
 
 const App = () => {
 
@@ -14,7 +14,7 @@ const App = () => {
       <CountdownTimer timeLeft={timeLeft} />
       <WordsContainer>
         <GeneratedWords words={words}/>
-        <Typing className="absolute inset-0" words={words} userInput={typed}/>
+        <Typing className="absolute inset-0 " words={words} userInput={typed}/>
       </WordsContainer>
       <Restart className="mx-auto mt-10 text-slate-500" onRestart={restart}/>
       <Results className="mt-10" state={state} errors={errors} accuracyPercentage={calculateAccuracyPercentage(errors, totalTyped)} total={totalTyped}/>
@@ -24,20 +24,25 @@ const App = () => {
 
 const WordsContainer = ({children}: {children:React.ReactNode}) => {
   return (
-    <div className="relative text-3xl max-w-2xl leading-relaxed break-all mt-3">
+    <div className="relative text-2xl max-w-4xl leading-relaxed break-normal mt-14 ">
       {children}
     </div>
   )
 }
 
   const GeneratedWords = ({ words } : { words: string }) => {
-  return  <div className="text-slate-500">
+  return  <div className="text-gray-500 px-10">
           {words}
           </div>
   }
 
   const CountdownTimer = ({ timeLeft } : { timeLeft: number }) => {
-    return <h2 className="text-primary-500 font-medium">Time: { timeLeft }</h2>
+    return (
+      <div className="grid text-center">
+        <img className="mx-auto h-20 w-20 " alt="sand-clock" src={Picture} />
+        <h2 className="text-yellow-400/90 text-xl bg-gray-800 rounded-2xl p-4 font-extrabold ">Time: { timeLeft }s</h2>
+      </div>
+    )
   }
 
 export default App
